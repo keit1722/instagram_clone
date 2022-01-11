84bb67c7233c124a5b098e5705ea375131e4c05f
keit1722 <keit1722@gmail.com>
2022-01-11 16:49:12 +0900

    [Add]be annotate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id               :bigint           not null, primary key
+#  crypted_password :string(255)
+#  email            :string(255)      not null
+#  salt             :string(255)
+#  username         :string(255)      not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email     (email) UNIQUE
+#  index_users_on_username  (username) UNIQUE
+#
+
 # Sorceryのジェネレータによって自動生成されたUserモデル
 
 class User < ApplicationRecord
