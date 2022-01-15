399749125a5178fce042fbd7050de9a3b4943092
keit1722 <keit1722@gmail.com>
2022-01-16 05:28:58 +0900

    [Add] 画像が表示されないためserializeを追記

--- app/models/post.rb
+++ app/models/post.rb
@@ -20,6 +20,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   mount_uploaders :images, PostImageUploader # imageカラムとPostImageUploaderクラスを紐付け
+  serialize :images, JSON
 
   validates :images, presence: true
   validates :body, presence: true, length: { maximum: 1000 }
