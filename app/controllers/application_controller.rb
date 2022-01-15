d8b0c0df76e5dd3a72de161928dedef1394f4298
keit1722 <keit1722@gmail.com>
2022-01-16 04:19:17 +0900

    [Modify]ログイン制御

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   # フラッシュメッセージのキーを追加
   add_flash_types :success, :info, :warning, :danger
+
+  # Sorceryのrequire_loginメソッドで使用されるnot_authenticatedメソッドをオーバーライド
+  def not_authenticated
+    redirect_to login_path, warning: 'ログインしてください'
+  end
 end
