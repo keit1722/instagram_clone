b68e634f522a1e744921b29d91e1ecaab5049437
keit1722 <keit1722@gmail.com>
2022-01-16 05:00:48 +0900

    [Modify]ルートパスを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
-  constraints ->(request) { request.session[:user_id].present? } do
-    # ログインしてる時のルートパス
-    root 'posts#index'
-  end
-  # ログインしてない時のルートパス
-  root 'user_sessions#new'
+  # require_loginメソッドを用意したのでここで条件分岐は不要
+  root 'posts#index'
 
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
-
+  
   resources :users, only: %i[new create]
   resources :posts
 end
