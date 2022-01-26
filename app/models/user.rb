942832be94ee85f39d434da9bf23cc6009c5d8f4
keit1722 <keit1722@gmail.com>
2022-01-26 21:40:09 +0900

    [Fix]失敗時に返り値にnilが返ってきたほうが使い勝手が良いので変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -67,7 +67,7 @@ class User < ApplicationRecord
 
   # relationshipsテーブルから引数で受け取ったother_userを削除、結果的にアンフォローする
   def unfollow(other_user)
-    active_relationships.find_by(followed_id: other_user.id).destroy
+    following.delete(other_user)
   end
 
   # followingの中に引数で渡されたpostが含まれているか否かをbooleanで返す、フォローしているかどうかを調べる
