35c4523d3b77e9b13b9d080a441bd4bb77bd4d4b
keit1722 <keit1722@gmail.com>
2022-02-01 23:09:14 +0900

    [Add]アバター用のカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_25_220813) do
+ActiveRecord::Schema.define(version: 2022_02_01_140609) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
     t.bigint "user_id"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2022_01_25_220813) do
     t.string "username", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
