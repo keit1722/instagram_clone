ad7275504f98db8ad428a7ccf30559d5281bd273
keit1722 <keit1722@gmail.com>
2022-02-05 00:35:34 +0900

    [Add]既読した場合のコントローラとモデルの処理を追加

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -21,9 +21,28 @@
 #  fk_rails_...  (user_id => users.id)
 #
 class Activity < ApplicationRecord
+  include Rails.application.routes.url_helpers # ビューやコントローラ以外でURLヘルパーを利用するときに必要な記述
   belongs_to :subject, polymorphic: true # ポリモーフィック関連付け
   belongs_to :user # userモデルに関連づけ
 
+  scope :recent, ->(count) { order(created_at: :desc).limit(count)} # 引数countの数だけ降順で表示させるscopeを定義
+
   # action_typeカラムで利用できるenumを定義
   enum action_type: { commented_to_own_post: 0, liked_to_own_post: 1, followed_me: 2 }
+  # readカラムで利用できるenumを定義
+  enum read: { unread: false, read: true }
+
+  def redirect_path # 条件によってリダイレクト先を決めるメソッドを定義
+    case action_type.to_sym # action_typeをシンボル化して比較オブジェクトに指定
+    when :commented_to_own_post
+      # コメントがあった通知だった場合の処理。コメントがあった投稿の詳細ベージへのパス、とコメントがある場所のアンカーを返している
+      post_path(subject.post, anchor: "comment-#{subject.id}")
+    when :liked_to_own_post
+      # いいねがあった通知だった場合の処理。いいねがあった投稿の詳細ベージへのパスを返している
+      post_path(subject.post)
+    when :followed_me
+      # フォローされた通知だった場合の処理。フォローをしてきたユーザの詳細ベージへのパスを返している
+      user_path(subject.follower)
+    end
+  end
 end
