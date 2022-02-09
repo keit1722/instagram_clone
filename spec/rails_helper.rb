75f8828e22f58a0b2e941ea374f383acce813d7f
keit1722 <keit1722@gmail.com>
2022-02-09 18:44:08 +0900

    [Add]FactoryBotの設定

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,4 +61,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.include FactoryBot::Syntax::Methods # インスタンス作成時にFactoryBotという記述を省略できるようになる設定
 end
